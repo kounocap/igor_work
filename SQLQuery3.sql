0ac08541d30cb3935da3a8b7b22bed7643887a8f
MrProha <mrprohagames@gmail.com>
2023-10-10 18:10:01 +0300

    pr2_done_2

---
+++ SQLQuery3.sql
@@ -0,0 +1,18 @@
+-- Updating datas --
+update STUDENT set SX='W' where ID_STUD=5;
+update TEACHER set SAL=200 where NAMETCH='Isaeva';
+SELECT *
+FROM STUDENT
+SELECT *
+FROM TEACHER
+-- Deleting parts of tables --
+delete from STUDENT where ID_STUD=4;
+delete from TEACHER where ID_Tch=4;
+SELECT *
+FROM STUDENT
+SELECT *
+FROM TEACHER
+SELECT *
+FROM COURSE
+SELECT *
+FROM CONTRACTY
